c08bf4594046f61154c902a5c18f343689dbbb9f
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-09-23 04:35:31 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -27,6 +28,7 @@ export default function App() {
 
 
       </Routes>
+      <Footer/>
       
     </div>
   )
